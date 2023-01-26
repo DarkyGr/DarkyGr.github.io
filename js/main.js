e438eac1b8b297a6614d56d60d93006c85885b5b
Guillermo Riaño <guilledark17@gmail.com>
2023-01-26 13:25:07 -0600

    Section Title SNS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let app = document.getElementById('typewriter');
 let app2 = document.getElementById('typewriter2');
- 
+let app3 = document.getElementById('typewriter3');
+
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 100,
@@ -10,17 +11,29 @@ let typewriter2 = new Typewriter(app2, {
   loop: true,
   delay: 100,
 });
- 
+
+let typewriter3 = new Typewriter(app3, {
+  loop: true,
+  delay: 100,
+});
+
 typewriter
   .pauseFor(500)
-  .typeString('Hi! 👋 I\'m Guillermo.')  
+  .typeString('Hi! 👋 I\'m Guillermo.')
   .pauseFor(500)
   .deleteChars(10)
   .start();
 
-  typewriter2
+typewriter2
   .pauseFor(500)
-  .typeString('Education & Experience')  
+  .typeString('Education & Experience')
+  .pauseFor(2000)
+  .deleteChars(10)
+  .start();
+
+  typewriter3
+  .pauseFor(500)
+  .typeString('Social Networking sites')
   .pauseFor(2000)
   .deleteChars(10)
   .start();
